a85f6d6b0b4ea50f9e03453ad487b5632492fa41
Mark Strain <markstrain@Marks-MBP.conwaycorp.net>
2021-02-08 19:30:00 -0600

    add heroku psql url

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///nba'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///nba')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret')
